9052bb336087954a17bc09eb7c32059e4ca89339
brajac709 <bij4@cornell.edu>
2019-12-20 22:51:42 -0500

    add bgg endpoint to node server

--- server/nodejs/server.js
+++ server/nodejs/server.js
@@ -1,6 +1,12 @@
 'use strict';
 const express = require('express');
 const path = require('path');
+const request = require('request');
+const xml2js = require('xml2js');
+const xpathm = require('xpath');
+const parse5 = require('parse5');
+const xmlser = require('xmlserializer');
+const dom = require('xmldom').DOMParser;
 
 var port = process.env.PORT || 1337;
 const app = express();
@@ -8,12 +14,43 @@ const app = express();
 // map HTML file extension to EJS renderer  (probably not needed here)
 // app.engine('html', require('ejs').renderFile);
 app.set('views', path.join(__dirname, '..', '..', 'client'));
+app.use('/', express.static(path.join(__dirname, '..','..', 'client')));
 
 app.get('/', function (req, res) {
     var options = {
         root: path.join(__dirname, '..', '..', 'client')
     }
     res.sendFile('index.html', options);
+});
+
+app.get('/bgg', function (req, res) {
+    var page, xpath;
+
+    if (typeof req.query.page === 'undefined') {
+        page = '1';
+    } else {
+        page = req.query.page;
+    }
+    if (typeof req.query.xpath === 'undefined') {
+        // xpath = '//table[@id="collectionitems"]//tr[@id="row_"]//td[@class="collection_thumbnail"]//a/@href';
+        xpath = '//x:table[@id="collectionitems"]//x:tr[@id="row_"]//x:td[@class="collection_thumbnail"]//x:a/@href';
+    } else {
+        xpath = req.query.page;
+    }
+
+    request.get('http://www.boardgamegeek.com/browse/boardgame/page/' + page,
+        function (err, resp, body) {
+
+            const document = parse5.parse(body);
+            const xhtml = xmlser.serializeToString(document);
+            const doc = new dom().parseFromString(xhtml);
+            const select = xpathm.useNamespaces({ "x": "http://www.w3.org/1999/xhtml" });
+            const nodes = select(xpath, doc);
+            const values = nodes.map(function (v) { return v.value });
+
+            res.json(values);
+
+        });
 });
 
 app.listen(port);
